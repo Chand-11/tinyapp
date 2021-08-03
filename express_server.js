e5123d44acf230ecaf1afb2abd31a14b40a23f92
chandni <chandnivaghasia11@gmail.com>
2021-08-02 20:56:46 -0400

    function generaterandome impelented

--- express_server.js
+++ express_server.js
@@ -2,12 +2,25 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 app.set("view engine", "ejs")
+const bodyParser = require("body-parser");
+//app.use(express.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: true}));
+
+const generateRandomString = () => {
+  return Math.random().toString(36).substr(2, 6);
+}; 
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
